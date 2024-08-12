49f786f8f6a4487f600066760316655578a676ed
Aditya Agrawal <89223187+Alpha19adi@users.noreply.github.com>
2024-08-13 04:37:17 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000'; // Adjust this to your backend URL
+const API_URL = 'https://amazon-backend.up.railway.app'; 
 
 const api = axios.create({
   baseURL: API_URL,
